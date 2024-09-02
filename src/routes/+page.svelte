88fc8bde78637816f321d7d52c753b720bc5895b
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-09-03 00:43:13 +0200

    update layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,53 @@
 <script lang="ts">
-	import { refreshValidator } from "$lib";
-	import type { ReportValidator } from "libra-reports/src/reports/validators";
-	import { onMount } from "svelte";
+	import { refreshValidator } from '$lib';
+	import type { ReportValidator } from 'libra-reports/src/reports/validators';
+	import { mapHandles } from 'libra-reports/src/util';
+	import { onMount } from 'svelte';
 
-  let isReady = false
-  let rv: ReportValidator;
-  onMount(async () => {
-    rv = await refreshValidator()
-    isReady = true
-  })
-
+	let isReady = false;
+	let rv: ReportValidator;
+	onMount(async () => {
+		rv = await refreshValidator();
+		isReady = true;
+	});
 </script>
 
 <body>
-  {#if isReady}
-    <table>
-      <tr>
-        <th>handle</th>
-        <th>address</th>
-        <th>bid</th>
-      </tr>
-        {#each rv.profiles.entries() as [k, v]}
-          <tr>
-            <td>{v.handle}</td>
-            <td>{k.slice(2,6)}</td>
-            <td>{v.bid_value}</td>
-          </tr>
-        {/each}
-      </table>
-  {:else}
-      ... living is waiting ...
-  {/if}
-
-
+	{#if isReady}
+		<table>
+			<tr><b>SEATED</b></tr>
+			<tr>
+				<th>handle</th>
+				<th>address</th>
+				<th>bid</th>
+				<th>active vouchers</th>
+				<th>vouches given</th>
+			</tr>
+			{#each rv.profiles.entries() as [k, v]}
+				{#if v.in_val_set}
+					<tr>
+						<td>{v.handle}</td>
+						<td>{k.slice(2, 6)}</td>
+						<td>{v.bid_value}</td>
+						<td>{mapHandles(v.active_vouchers ?? [], rv)}</td>
+						<td>{mapHandles(v.vouches_given?.addresses ?? [], rv)}</td>
+					</tr>
+				{/if}
+			{/each}
+			<tr><b> NOT SEATED</b></tr>
+			{#each rv.profiles.entries() as [k, v]}
+				{#if !v.in_val_set}
+					<tr>
+						<td>{v.handle}</td>
+						<td>{k.slice(2, 6)}</td>
+						<td>{v.bid_value}</td>
+						<td>{mapHandles(v.active_vouchers ?? [], rv)}</td>
+						<td>{mapHandles(v.vouches_given?.addresses ?? [], rv)}</td>
+					</tr>
+				{/if}
+			{/each}
+		</table>
+	{:else}
+		... living is waiting ...
+	{/if}
 </body>
